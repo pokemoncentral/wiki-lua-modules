ee7c7d5cdfa327d345b42879095134cefd5bce77
Flavio <flavio.a.618@gmail.com>
2018-05-31 19:27:02 +0200

    Added interface in PokémonData to get Pokémon's type from name or ndex

--- PokémonData.lua
+++ PokémonData.lua
@@ -8,21 +8,25 @@ local b = {}
 
 -- Data modules aren't loaded here because they probably won't be used
 
+local mw = require('mw')
+
 local txt = require('Modulo:Wikilib/strings')
 local data
 local abils
+local forms
 
 --[[
 
-Ritorna la table dati passata come argomento
-se questa è già stata caricata, altrimenti
-ritorna il modulo passato come secondo argomento
+Returns the passed table if it's already been loaded, otherwise the module
+named after the second parameter is loaded and returned. The module is loaded
+with mw.loadData unless the third parameter is true
 
 --]]
 
-local loadData = function(tab, moduleName)
+local loadData = function(tab, moduleName, useRequire)
 	if not tab then
-		return mw.loadData('Modulo:' .. moduleName)
+		local loadFunction = useRequire and require or mw.loadData
+		return loadFunction('Modulo:' .. moduleName)
 	end
 	return tab
 end
@@ -36,14 +40,14 @@ argomento
 --]]
 
 local getAbil = function(name, abilityNumber)
-	abils = loadData(abils, 'PokéAbil/data').pokeabil
+	abils = loadData(abils, 'PokéAbil/data')
 	return abils[tonumber(name) or name]['ability' .. abilityNumber]
 end
 
 -- Ritorna il dex nazionale dato il nome
 
 b.getNdex = function(frame)
-	data = loadData(data, 'Poké/data').pokedata
+	data = loadData(data, 'Poké/data')
 	return string.tf(data[string.trim(frame.args[1] or ''):lower()].ndex)
 end
 
@@ -52,7 +56,7 @@ b.get_ndex = b.getNdex
 -- Ritorna il nome dato il dex nazionale
 
 b.getName = function(frame)
-	data = loadData(data, 'Poké/data').pokedata
+	data = loadData(data, 'Poké/data')
 	return data[tonumber(string.trim(frame.args[1] or ''))].name
 end
 
@@ -84,6 +88,38 @@ b.get_abil_d, b.get_abil_h, b.get_abil_n = b.getAbild, b.getAbild, b.getAbild
 
 --[[
 
+Returns a Pokémon's type (specified by the second parameter) given its name or
+ndex. Transform 'coleot' into 'coleottero'
+
+--]]
+
+local getType = function(name, typeNumber)
+	data = loadData(data, 'Poké/data')
+
+	local pkmnType = data[tonumber(name) or name]['type' .. typeNumber]
+	pkmnType = pkmnType == 'coleot' and 'coleottero' or pkmnType
+	return pkmnType
+end
+
+-- Returns a Pokémon's first type given its name or ndex
+
+b.getType1 = function(frame)
+	return getType(string.trim(frame.args[1] or ''):lower(), '1')
+end
+
+b.get_type_1 = b.getType1
+
+-- Returns a Pokémon's second type given its name or ndex. If the Pokémon has
+-- only one type, it returns the first type instead
+
+b.getType2 = function(frame)
+	return getType(string.trim(frame.args[1] or ''):lower(), '2')
+end
+
+b.get_type_2 = b.getType2
+
+--[[
+
 Frontend for wikilib/forms.getlink: returns the link
 to an'alternate form from module AltForms/data or
 UselessForms/data.
@@ -94,7 +130,8 @@ argument is a flag to get black or normal link
 --]]
 
 b.getLink = function(frame)
-	local forms = require('Modulo:Wikilib/forms')
+	forms = loadData(forms, 'Wikilib/forms', true)
+
 	local name, black = string.trim(frame.args[1]), string.trim(frame.args[2])
 	local link = forms.getLink(name, black)
 	if link ~= '' then
