c22b084f9ccf94099db8db8c1d28ec045a590dc1
Davide Laezza <truzzialrogo@gmx.com>
2018-06-02 12:15:27 +0200

    Fixing small stuff in DebRes + more tests

--- test/DebRes.spec.lua
+++ test/DebRes.spec.lua
@@ -6,7 +6,28 @@ local debRes = require('DebRes').debRes
 print(debRes{args={'Luxio'}})
 
 -- Immunities
-print(debRes{args={'Mudsdale'}})
+print(debRes{args={'Shuppet'}})
+
+-- Multiple ability
+print(debRes{args={'Dewgong'}})
+
+-- Single Ability
+print(debRes{args={'Duskull'}})
+
+-- Immunity by both type and ability
+print(debRes{args={'Rhydon'}})
+
+-- Filtro, Solidroccia & co
+print(debRes{args={'Mr. Mime'}})
+
+-- Filtro only
+print(debRes{args={'Aggron'}})
 
 -- Multiple forms collapse into one
 print(debRes{args={'Scizor'}})
+
+-- Some forms collapse
+print(debRes{args={'Charizard'}})
+
+-- No forms collapse
+print(debRes{args={'Altaria'}})
