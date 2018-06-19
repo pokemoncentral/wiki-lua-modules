2b054e98b61c939e23f97516db036ac2ce754e85
Davide Laezza <truzzialrogo@gmx.com>
2018-06-19 11:57:03 +0200

    Card layout for Typelist

--- Typelist.lua
+++ Typelist.lua
@@ -1,9 +1,7 @@
 --[[
 
-This module prints the list of all Pokémon,
-alternative forms included, having a given
-type. It divides them into mono-typed,
-first-typed and second-typed.
+This module prints the list of all Pokémon, alternative forms included, having
+a given type. It divides them into mono-typed, first-typed and second-typed.
 
 --]]
 
@@ -22,48 +20,36 @@ local pokes = require('Poké-data')
 
 --[[
 
-Base class for specialized list entry
-(mono-, first- and second-typed): it is
-not utilizable directly because it does
-not filter the type.
+Base class for specialized list entry (mono-, first- and second-typed): it is
+not utilizable directly because it does not filter the type.
 
 --]]
 g.Entry = oop.makeClass(list.PokeSortableEntry)
 
 -- Typebox-related constants
 g.Entry.typeBox = {
-    classes = '-5 flex flex-row flex-main-center flex-items-center',
-    styles = 'height: 100%; padding: 0.5ex 0.3em;'
+    classes = '-5 text-center',
+    styles = 'padding: 0.5ex 0.3em;'
 }
 
---[[
-
-Returns the heading line for tables, given
-type, heading level and ending.
-
---]]
+-- Returns the heading line for tables, given type, heading level and ending.
 g.Entry.makeHeader = function(type, level, ending)
     type = type == 'coleot' and 'Coleottero' or string.fu(type)
     local headerTags = string.rep('=', level)
-    return table.concat({headerTags, 'Pokémon di tipo',
-            type, ending, headerTags}, ' ')
+    return table.concat({headerTags, 'Pokémon di tipo', type, ending,
+            headerTags}, ' ')
 end
 
 -- Returns wikicode for typeboxes
 g.Entry.makeTypeBox = function(type)
-    return box.boxTipoLua(
-            string.fu(type),
-            g.Entry.typeBox.classes,
-            g.Entry.typeBox.styles
-    )
+    return box.boxTipoLua(string.fu(type), g.Entry.typeBox.classes,
+        g.Entry.typeBox.styles)
 end
 
 --[[
 
-Constructor: first argument is an entry
-from Poké/data, second one its key.
-Subclasses are to return nil, as specified
-by makeList in Wikilib/lists.
+Constructor: first argument is an entry from Poké/data, second one its key.
+Subclasses are to return nil, as specified by makeList in Wikilib/lists.
 
 --]]
 g.Entry.new = function(pokeData, name)
@@ -74,25 +60,26 @@ end
 
 --[[
 
-Wikicode for a list entry: Pokémon ndex,
-mini sprite, name and types. Mono-typed
-Pokémon display the type only once.
+Wikicode for a list entry: Pokémon ndex, mini sprite, name and types.
+Mono-typed Pokémon display the type only once.
 
 --]]
 g.Entry.__tostring = function(this)
-    return string.interp([=[| class="hidden-xs" | ${ndex}
-| ${static}
-| class="hidden-xs" style="padding: 0.5ex 0.5em;" | [[${name}]]${form}
-| style="height: 100%; padding: 1.2ex 0.3ex;" | ${type1}${type2}]=],
+    local monoType = this.type1 == this.type2
+    return string.interp([=[| class="min-width-xs-20" | ${ndex}
+| class="min-width-xs-20" | ${static}
+| class="min-width-xs-60" style="padding: 0.5ex 0.5em;" | [[${name}]]${form}
+| class="min-width-xs-${xsWidth}" style="padding: 1.2ex 0.3ex;" | ${type1}${type2}]=],
     {
         ndex = this.ndex and string.tf(this.ndex) or '???',
         static = ms.staticLua(string.tf(this.ndex or 0) ..
                 (this.formAbbr == 'base' and '' or this.formAbbr or '')),
         name = this.name,
         form = this.formsData and this.formsData.links[this.formAbbr] or '',
+        xsWidth = 70 / (monoType and 1 or 2),
         type1 = g.Entry.makeTypeBox(this.type1),
-        type2 = this.type1 == this.type2 and ''
-                or ' || style="height: 100%; padding: 1.2ex 0.3ex;" | '
+        type2 = monoType and ''
+                or ' || class="min-width-xs-35" style="padding: 1.2ex 0.3ex;" | '
                         .. g.Entry.makeTypeBox(this.type2)
     })
 end
@@ -107,12 +94,10 @@ end
 
 --[[
 
-Constructor: the first argument is an entry
-from Poké/data, the second one its key and
-the third is the type the Pokémon must have.
-As specified by makeList in Wikilib/lists,
-returns nil whenever the Pokémon is either
-dual-typed or not of the passed type.
+Constructor: the first argument is an entry from Poké/data, the second one its
+key and the third is the type the Pokémon must have. As specified by makeList
+in Wikilib/lists, returns nil whenever the Pokémon is either dual-typed or not
+of the passed type.
 
 --]]
 g.MonoTypeEntry.new = function(pokeData, name, type)
@@ -136,11 +121,9 @@ end
 
 --[[
 
-Constructor: the first argument is an entry
-from Poké/data, the second one its key and
-the third is the type the Pokémon must have
-as first. As specified by makeList in Wikilib/lists,
-returns nil whenever the Pokémon is either
+Constructor: the first argument is an entry from Poké/data, the second one its
+key and the third is the type the Pokémon must have as first. As specified by
+makeList in Wikilib/lists, returns nil whenever the Pokémon is either
 mono-typed or its first type is not the passed one.
 
 --]]
@@ -165,11 +148,9 @@ end
 
 --[[
 
-Constructor: the first argument is an entry
-from Poké/data, the second one its key and
-the third is the type the Pokémon must have
-as second. As specified by makeList in Wikilib/lists,
-returns nil whenever the Pokémon is either
+Constructor: the first argument is an entry from Poké/data, the second one its
+key and the third is the type the Pokémon must have as second. As specified by
+makeList in Wikilib/lists, returns nil whenever the Pokémon is either
 mono-typed or its second type is not the passed one.
 
 --]]
@@ -185,16 +166,16 @@ end
 
 --[[
 
-Wikicode for list header: it takes the type
-name, for colors, and the number of types,
-to print the correct amount of type columns.
+Wikicode for list header: it takes the type name, for colors, and the number of
+types, to print the correct amount of type columns.
 
 --]]
 local makeHeader = function(type, typesCount)
     return string.interp([=[{| class="roundy sortable pull-center text-center roundy-footer white-rows" style="border-spacing: 0; padding: 0.3ex; ${bg};"
-! class="hidden-xs" style="padding-top: 0.5ex; padding-bottom: 0.5ex; padding-left: 0.5ex;" | [[Elenco Pokémon secondo il Pokédex Nazionale|<span style="color:#000">#</span>]]
-! <span class="hidden-xs">&nbsp;</span><span class="visible-xs">Pokémon</span>
-! class="hidden-xs" | [[Pokémon|<span style="color:#000">Pokémon</span>]]
+|- class="hidden-xs"
+! style="padding-top: 0.5ex; padding-bottom: 0.5ex; padding-left: 0.5ex;" | [[Elenco Pokémon secondo il Pokédex Nazionale|<span style="color:#000">#</span>]]
+! &nbsp;
+! [[Pokémon|<span style="color:#000">Pokémon</span>]]
 ${types}]=],
 {
     bg = css.horizGradLua{type = type},
@@ -206,14 +187,10 @@ end
 
 --[[
 
-Creates heading and HTML table for
-Pokémons of a given type. Its first
-argument is such type, the second one
-an Entry class complying to makeList
-in Wikilib/listm and the third is the
-heading line. The latter argument is
-optional, defaulting to the return
-of the Entry class makeHeader method.
+Creates heading and HTML table for Pokémons of a given type. Its first argument
+is such type, the second one an Entry class complying to makeList in
+Wikilib/listm and the third is the heading line. The latter argument is
+optional, defaulting to the return of the Entry class makeHeader method.
 
 --]]
 g.makeTypeTable = function(type, Entry, header)
@@ -225,18 +202,17 @@ g.makeTypeTable = function(type, Entry, header)
             makeEntry = Entry.new,
             header = makeHeader(type,
                     Entry == g.MonoTypeEntry and 1 or 2),
+            separator = '|- class="roundy flex-xs flex-row flex-wrap flex-main-center flex-items-center" style="margin: 0.5rem 0;"',
             footer = '|}'
         })}, '\n')
 end
 
 --[[
 
-Wikicode interface function: takes a type
-as the title of its page ('<type>')
-and prints a list fo all the Pokémon having
-such type, dividing them into mono-typed,
-first-typed and second-typed. Heading for
-sublists are also displayed.
+Wikicode interface function: takes a type as the title of its page ('<type>')
+and prints a list fo all the Pokémon having such type, dividing them into
+mono-typed, first-typed and second-typed. Heading for sublists are also
+displayed.
 
 Examples:
 {{#invoke: Typelist | Typelist | Ghiaccio }}
@@ -262,8 +238,7 @@ g.typelist = function(frame)
         }, '\n\n')
 end
 
-g.Typelist, g.TypeList, g.typeList = g.typelist,
-        g.typelist, g.typelist
+g.Typelist, g.TypeList, g.typeList = g.typelist, g.typelist, g.typelist
 
 -- print(g.typelist{args={arg[1]}})
 
---
+++ test/Typelist.spec.lua
@@ -0,0 +1,6 @@
+-- "Test cases" for Typelist
+
+local typelist = require('Typelist').typelist
+
+-- No real corner cases exist
+print(typelist{args={'Elettro'}})
