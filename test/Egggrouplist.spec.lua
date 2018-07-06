b13bdd2e1bf8ecc1022de47823224aeede5fa73b
Davide Laezza <truzzialrogo@gmx.com>
2018-07-07 01:32:00 +0200

    Tests for Egggrouplist

---
+++ test/Egggrouplist.spec.lua
@@ -0,0 +1,6 @@
+-- "Test cases" for Egggrouplist
+
+local g = require('Egggrouplist')
+
+print(g.singlegrouplist{args={'Acqua 1 (gruppo uova)'}})
+print(g.doublegrouplist{args={'Acqua 1 (gruppo uova)'}})
