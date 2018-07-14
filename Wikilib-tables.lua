39e3a0677a5a8b82c448ee83478c82d8265dafef
Davide Laezza <truzzialrogo@gmx.com>
2018-07-14 14:16:08 +0200

    Started refactoring of Wikilib-tables: English comments and alphabetic order

--- Wikilib-tables.lua
+++ Wikilib-tables.lua
@@ -3,26 +3,17 @@
 
 local t = {}
 
+
 -- Stateless iterator on non-integer keys
 local nextNonInt = function(tab, key)
     local nextKey, nextValue = key
     repeat
         nextKey, nextValue = next(tab, nextKey)
-    until type(nextKey) ~= 'number'
-            or math.floor(nextKey) ~= nextKey
+    until type(nextKey) ~= 'number' or math.floor(nextKey) ~= nextKey
     return nextKey, nextValue
 end
+local next_non_int = nextNonInt     -- luacheck: no unused
 
-local next_non_int = nextNonInt -- luacheck: no unused
-
--- Stateless iterator to be used in for loops
-table.nonIntPairs = function(tab)
-    return nextNonInt, tab
-end
-
-table.non_int_pairs = t.nonIntPairs
-t.nonIntPairs, t.non_int_pairs =
-        table.nonIntPairs, table.nonIntPairs
 
 -- Returns true only if a is lexigographically greater than b
 local minor = function(a, b)
@@ -30,10 +21,7 @@ local minor = function(a, b)
         return true
     end
 
-    --[[
-        Necessario affinché roba come 1 e
-        '01' siano considerati uguali
-    --]]
+    -- Converting to number for better comparison (eg. '01' and 1)
     local nA, nB = tonumber(a), tonumber(b)
     if nA and nB then
         return nA < nB
@@ -42,34 +30,58 @@ local minor = function(a, b)
     return tostring(a) < tostring(b)
 end
 
+
 --[[
 
-Ricerca lineare: si passa una table e un valore e
-restituisce il suo indice o nil
+Returns true if all of the elements in the list satisties a predicate.
+
+The predicate function takes the value as the first argument, then the key;
+usual key-value order is reversed because the key is often unnecessary, and
+having it last leads to nicer syntax in the call site.
+
+TODO: refactoring to use just built-ins
 
 --]]
-table.search = function(tab, value)
-    for k, v in pairs(tab) do
-        if v == value then
-            return k
-        end
-    end
-    -- No explicit return, thus nil
+table.all = function(tab, funct, iter)
+     return table.fold(tab, true,
+        function(acc, value, key)
+            return funct(value, key) and acc
+        end, iter)
 end
 
-table.linearSearch, table.linear_search =
-        table.search, table.search
-t.search, t.linearSearch, t.linear_search =
-        table.search, table.search, table.search
+t.all = table.all
+
 
 --[[
 
-Ricerca ricorsiva: cerca un valore in una table e
-in eventuali subtables. Ritorna una lista di indici,
-l'ultimo per l'elemento, i precedenti per le subtables.
-Se l'elemento non è presente, ritorna nil
+Returns true if at least one of the elements in the list satisties a predicate.
 
-Esempio: t.deep_search({'a', {'b', 'c'}}, 'b') --> 2, 1
+The predicate function takes the value as the first argument, then the key;
+usual key-value order is reversed because the key is often unnecessary, and
+having it last leads to nicer syntax in the call site.
+
+TODO: refactoring to use just built-ins
+
+--]]
+table.any = function(tab, funct, iter)
+    return table.fold(tab, false,
+        function(acc, value, key)
+            return funct(value, key) or acc
+        end, iter)
+end
+t.any = table.any
+
+
+--[[
+
+Recursive search: looks for a value in a table and possible subtables. Returns
+a list of indices, mapping the nesting of tables. This means that the last
+index is the one of the element, the second last is the subtable it is found
+in, the third last the subtable that contains such table an so on. If the
+element is not found, returns nil.
+
+Example:
+table.deep_search({'a', {'b', 'c'}}, 'b') --> 2, 1
 
 --]]
 table.deepSearch = function(tab, value)
@@ -90,36 +102,177 @@ table.deepSearch = function(tab, value)
     end
     -- No explicit return, thus nil
 end
+table.deep_search = table.deepSearch
+t.deepSearch, t.deep_search = table.deepSearch, table.deepSearch
 
-table.deep_search, table.recursiveSearch, table.recursive_search =
-        table.deepSearch, table.deepSearch, table.deepSearch
-t.deepSearch, t.deep_search, t.recursiveSearch, t.recursive_search =
-        table.deepSearch, table.deepSearch, table.deepSearch, table.deepSearch
 
 --[[
 
-Ritorna il numero di elementi di una table,
-solo quelli con indici numerici se il secondo
-argomento è true o 'num', altrimenti anche
-quelli con indice diverso.
+Applies a function over all of the elements of a table returned by the passed
+iterator. The function is expected to return a list. The lists produced from
+the elements are merged together in the same way as table.merge would do. The
+iterator defaults to pairs.
+
+The function takes as input an element and its ke, in this order: in fact, the
+key is often unnecessary, and would just clutter the code in these cases.
+
+--]]
+table.flatMap = function(tab, funct, iter)
+    iter = iter or pairs
+
+    local dest = {}
+    for k, v in iter(tab) do
+        dest = table.merge(dest, funct(v, k))
+    end
+    return dest
+end
+table.flat_map = table.flatMap
+t.flatMap, t.fat_map = table.flatMap, table.flatMap
+
+
+--[[
+
+Applies a function over all of the elements of a table returned by the passed
+iterator. The function is expected to return a list. The lists produced from
+the elements are converted to numeric lists: the order of the elements is
+determined by the given iterator. These lists are then concatenated together,
+in the order the elements of the initial list are returned by the iterator.
+
+The function takes as input an element and its ke, in this order: in fact, the
+key is often unnecessary, and would just clutter the code in these cases.
+
+--]]
+table.flatMapToNum = function(tab, funct, iter)
+    iter = iter or pairs
+
+    local dest = {}
+    for k, v in iter(tab) do
+        for _, value in iter(funct(v, k)) do
+            table.insert(dest, value)
+        end
+    end
+    return dest
+end
+table.flat_map_to_num = table.flatMapToNum
+t.flatMapToNum, t.flat_map_to_num = table.flatMapToNum, table.flatMapToNum
+
+
+--[[
+
+Classic fold on lists: applies a binary function to an accumulator and a
+value-key pair taken from the list and returns the final result. The iterator
+determines the order elements are scanned, and therefore the final result.
+
+The function takes the accumulator as the first argument, then the value and
+finally the key; usual key-value order is reversed because the key is often
+unnecessary, and having it last leads to nicer syntax in callers.
+
+--]]
+table.fold = function(tab, zero, func, iter)
+    iter = iter or pairs
+
+    for key, value in iter(tab) do
+        zero = func(zero, value, key)
+    end
+
+    return zero
+end
+t.fold = table.fold
+
+
+--[[
+
+Returns the number of elements in a table. It should only be used when
+Scribunto overrides the # operator so that it doesn't work propertly.
+If the second argument is 'num' it only counts items having a numeric index,
+otherwise it takes all elements in account.
 
 --]]
 table.getn = function(self, count)
     count = count or 'all'
+
     local n = 0
-    local iterator
-    if count == true or tostring(count):lower() == 'num' then
-        iterator = ipairs
-    else
-        iterator = pairs
-    end
+    local iterator = (count == true or tostring(count):lower() == 'num')
+    and ipairs or pairs
+
     for _ in iterator(self) do
         n = n + 1
     end
+
     return n
 end
-
 t.getn = table.getn
+
+
+--[[
+
+Applies a function to the elements of a table scanned by the passed iterator.
+It returls a table holding the results of the function application, bound to
+the same keys as the original items. If no iterator is passed, all elements
+are scanned.
+
+The function takes as input an element and its ke, in this order: in fact, the
+key is often unnecessary, and would just clutter the code in these cases.
+
+--]]
+table.map = function(tab, funct, iter)
+    iter = iter or pairs
+
+    local dest = {}
+    for key, value in iter(tab) do
+        dest[key] = funct(value, key)
+    end
+    return dest
+end
+t.map = table.map
+
+
+--[[
+
+Applies a function to the elements of a table scanned by the passed iterator.
+It returls a table holding the results of the function application, bound to
+numeric keys in the order the original elements are returned by the iterator.
+If no iterator is passed, all elements are scanned.
+
+The function takes as input an element and its ke, in this order: in fact, the
+key is often unnecessary, and would just clutter the code in these cases.
+
+--]]
+table.mapToNum = function(tab, funct, iter)
+    iter = iter or pairs
+
+    local dest = {}
+    for key, value in iter(tab) do
+        table.insert(dest, funct(value, key))
+    end
+    return dest
+end
+table.map_to_num = table.mapToNum
+t.mapToNum, t.map_to_num = table.mapToNum, table.mapToNum
+
+
+-- Stateless iterator to be used in for loops
+table.nonIntPairs = function(tab)
+    return nextNonInt, tab
+end
+table.non_int_pairs = t.nonIntPairs
+t.nonIntPairs, t.non_int_pairs = table.nonIntPairs, table.nonIntPairs
+
+
+-- Linear search. Returns the index of the passed value if found, else nil.
+table.search = function(tab, value)
+    for k, v in pairs(tab) do
+        if v == value then
+            return k
+        end
+    end
+    -- No explicit return, thus nil
+end
+t.search = table.search
+
+
+
+
 
 --[[
 
@@ -147,182 +300,6 @@ table.table_keys_alias, table.keysAlias, table.keys_alias =
         table.tableKeysAlias, table.tableKeysAlias, table.tableKeysAlias
 t.tableKeysAlias, t.table_keys_alias, t.keysAlias, t.keys_alias =
         table.tableKeysAlias, table.tableKeysAlias, table.tableKeysAlias, table.tableKeysAlias
-
---[[
-
-Classic fold on lists: applies a binary
-function to an accumulator and a value-key
-pair taken from the list and returns the
-final result. The iterator determines the
-order elements are scanned, and therefore
-the final result.
-
-The function takes the accumulator as the
-first argument, then the value and finally
-the key; usual key-value order is reversed
-because the key is often unnecessary, and
-having it last leads to nicer syntax in callers.
-
---]]
-table.fold = function(tab, zero, func, iter)
-    iter = iter or pairs
-
-    for key, value in iter(tab) do
-        zero = func(zero, value, key)
-    end
-
-    return zero
-end
-
-t.fold = table.fold
-
---[[
-
-Applica una funzione agli elementi di una table
-restituiti dall'iteratore dato, ritornandone un'
-altra con i risultati aventi la stessa chiave
-dell'elemento dato in ingresso alla funzione. Se
-non è passato nessun iteratore, si scorrono tutti
-gli elementi della table sorgente.
-
-La funzione riceve in ingresso un elemento e la sua
-chiave, in quest'ordine perché la chiave non è sempre
-necessaria e sarebbe fastidioso avere un argomento
-placeholder.
-
---]]
-table.map = function(tab, funct, iter)
-    iter = iter or pairs
-
-    local dest = {}
-    for key, value in iter(tab) do
-        dest[key] = funct(value, key)
-    end
-    return dest
-end
-
-t.map = table.map
-
---[[
-
-Applies a function over all of the elements of a table returned by the passed
-iterator. The function is expected to return a list. The lists produced from
-the elements are merged together in the same way as table.merge would do. The
-iterator defaults to pairs.
-
-The function takes as input an element and its ke, in this order: in fact, the
-key is often unnecessary, and would just clutter the code in these cases.
-
---]]
-table.flatMap = function(tab, funct, iter)
-    iter = iter or pairs
-
-    local dest = {}
-    for k, v in iter(tab) do
-        dest = table.merge(dest, funct(v, k))
-    end
-    return dest
-end
-
-table.flat_map = table.flatMap
-t.flatMap, t.fat_map = table.flatMap, table.flatMap
-
---[[
-
-Applica una funzione agli elementi di una table
-restituiti dall'iteratore dato, ritornandone un'
-altra con i risultati aventi chiavi numeriche
-nell'ordine proprio dell'iteratore. Se non ne è
-passato nessuno, si scorrono tutti gli elementi
-della table sorgente.
-
-La funzione riceve in ingresso un elemento e la sua
-chiave, in quest'ordine perché la chiave non è sempre
-necessaria e sarebbe fastidioso avere un argomento
-placeholder.
-
---]]
-table.mapToNum = function(tab, funct, iter)
-    iter = iter or pairs
-
-    local dest = {}
-    for key, value in iter(tab) do
-        table.insert(dest, funct(value, key))
-    end
-    return dest
-end
-
-table.map_to_num, table.mapToNumeric, table.map_to_numeric
-        = table.mapToNum, table.mapToNum, table.mapToNum
-t.mapToNum, t.map_to_num, t.mapToNumeric, t.map_to_numeric
-        = table.mapToNum, table.mapToNum, table.mapToNum,
-            table.mapToNum
-
---[[
-
-Applies a function over all of the elements of a table returned by the passed
-iterator. The function is expected to return a list. The lists produced from
-the elements are converted to numeric lists: the order of the elements is
-determined by the given iterator. These lists are then concatenated together,
-in the order the elements of the initial list are returned by the iterator.
-
-The function takes as input an element and its ke, in this order: in fact, the
-key is often unnecessary, and would just clutter the code in these cases.
-
---]]
-table.flatMapToNum = function(tab, funct, iter)
-    iter = iter or pairs
-
-    local dest = {}
-    for k, v in iter(tab) do
-        for _, value in iter(funct(v, k)) do
-            table.insert(dest, value)
-        end
-    end
-    return dest
-end
-
-table.flat_map_to_num, table.flatMapToNumeric, table.flat_map_to_numeric
-        = table.flatMapToNum, table.flatMapToNum, table.flatMapToNum
-t.flatMapToNum, t.flat_map_to_num, t.flatMapToNumeric, t.flat_map_to_numeric
-        = table.flatMapToNum, table.flatMapToNum, table.flatMapToNum,
-            table.flatMapToNum
-
---[[
-
-Returns true if at least one of the elements in the list satisties a predicate.
-
-The predicate function takes the value as the first argument, then the key;
-usual key-value order is reversed because the key is often unnecessary, and
-having it last leads to nicer syntax in the call site.
-
---]]
-table.any = function(tab, funct, iter)
-    return table.fold(tab, false,
-        function(acc, value, key)
-            return funct(value, key) or acc
-        end, iter)
-end
-
-t.any = table.any
-
---[[
-
-Returns true if all of the elements in the list satisties a predicate.
-
-The predicate function takes the value as the first argument, then the key;
-usual key-value order is reversed because the key is often unnecessary, and
-having it last leads to nicer syntax in the call site.
-
---]]
-table.all = function(tab, funct, iter)
-     return table.fold(tab, true,
-        function(acc, value, key)
-            return funct(value, key) and acc
-        end, iter)
-end
-
-t.all = table.all
 
 --[[
 
@@ -609,10 +586,8 @@ t.slice = table.slice
 
 --[[
 
-Ritorna una copia di una table di un modulo
-caricato con mw.loadData. La table ritornata,
-essendo una copia, non è read-only, ed è quindi
-possibile usare su di essa la table library
+Deep clones a table. It intentionally doesn't clone the metatables, as that's
+the job of mw.clone.
 
 --]]
 table.copy = function(value)
@@ -626,9 +601,7 @@ table.copy = function(value)
 end
 
 t.copy = table.copy
-table.cloneLoadData, table.clone_load_data =
-    table.copy, table.copy
-t.cloneLoadData, t.clone_load_data =
-    table.copy, table.copy
+table.cloneLoadData, table.clone_load_data = table.copy, table.copy
+t.cloneLoadData, t.clone_load_data = table.copy, table.copy
 
 return t
