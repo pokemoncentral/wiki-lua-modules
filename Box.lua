6b4f54330619710e4a1a8221042e4c6d174d3f7f
Davide Laezza <truzzialrogo@gmx.com>
2018-07-23 18:22:17 +0200

    Commented new implementation of Box module

--- Box.lua
+++ Box.lua
@@ -4,7 +4,61 @@ This module contains utility code for boxes: these are a link in a rounded
 box, with a left-to-right gradient from the dark shade of a color to the
 normal shade of the same color.
 
-Shorthands are provided for boxes that display a type and lists of such boxes.
+The basic function is called box, and has the following arguments:
+
+- 1: displayed text.
+- 2: link target, Defaults to text.
+- 3: gradient color, from dark to normal, right-to-left.
+- 4: Space-spearated string of predefined configurations names. Optional,
+    defaults to no predefined configurations.
+- 5: Space-separated list of CSS classes. Optional, defaults to no CSS classes.
+- 6: CSS styles, in the format of an HTML style attribute values. Optional,
+    defaults to no CSS styles.
+- 7: Text color, defaults to #FFFFFF
+
+Example call:
+
+{{#invoke | Box | box | erba | erba (gruppo uova) | erba_uova | thin |
+    inline-block width-sm-60 | padding: 2ex; margin-top: 0.5ex; | FFFFFF }}
+
+There is also a number of shorthands, with a rather similar call. They are
+listed below:
+
+- TypeBox: a box displaying a type, with its color as background. Arguments:
+
+    - 1: The type. Defaults to 'Sconosciuto'.
+    - 2: Space-spearated string of predefined configurations names. Optional,
+        defaults to no predefined configurations.
+    - 3: Space-separated list of CSS classes. Optional, defaults to no CSS
+        classes.
+    - 4: CSS styles, in the format of an HTML style attribute values. Optional,
+        defaults to no CSS styles.
+
+    Example: {{#invoke | Box | typeBox | Elettro | thick | | margin: 3em; }}
+
+- EggBox: a box displaying an egg grup, in its color as a background. Arguments:
+
+    - 1: The egg group, without ' (gruppo uova)' suffix. Defaults to
+        'Sconosciuto (gruppo uova)'.
+    - 2: Space-spearated string of predefined configurations names. Optional,
+        defaults to no predefined configurations.
+    - 3: Space-separated list of CSS classes. Optional, defaults to no CSS
+        classes.
+    - 4: CSS styles, in the format of an HTML style attribute values. Optional,
+        defaults to no CSS styles.
+
+    Example: {{#invoke | Box | eggBox | Erba | | inline-block | margin: 3em; }}
+
+In addition to the single box versions, also multiple box version are provided
+for the shorthands. Their name is the same as the shorthand, with 'box'
+replaced by 'list'. They are invoked the same way, except that the first
+argument is a comma-separated list of types, egg groups, etc. The other
+arguemnts are applied to al generated boxes. Some example calls below:
+
+- typeList: {{#invoke | Box | typeList | Elettro, Terra, Acciaio | |
+    inline-block | margin: 3em; }}
+
+- eggList: {{#invoke | Box | eggList | Acqua 1, Campo, Magico | thin }}
 
 --]]
 
@@ -82,7 +136,7 @@ local export = function(name, suffix, luaFunction, wikicodeFunction)
     local luaNames = {table.concat{name, string.fu(suffix), 'Lua'},
             table.concat{name, '_', suffix, '_lua'}}
     local wikicodeNames = {name .. string.fu(suffix),
-            string.fu(name) .. string.fu(suffix)}
+            string.fu(name .. suffix)}
 
     -- Aliases-dependent names (eg, boxTipoLua)
     if nameAliases then
@@ -119,12 +173,44 @@ local makeWikicodeIntreface = function(luaFunction)
     end
 end
 
+--[[
+
+Shorthands are wrapper around the main b.boxLua function. Generally, they
+derive the arguments to the latter from a smaller set of arguments: for
+example, a box displaying a type has the same displayed text, link target and
+color, that is the name of the type itself.
+
+In this table, every element is a function that from arbitrary arguments
+returns the b.boxLua arguments.
+
+--]]
 b.shortHands = {
+    --[[
+        Box displaying a type. Arguments:
+            - type: The type, defaults to 'Sconosciuto'
+            - pdfs: Table or space-spearated string of predefined
+                configurations names. Optional, defaults to {}.
+            - classes: Table/string of CSS classes, in the format parseClasses
+                and printClasses produce respectively. Optional, defaults to {}.
+            - styles: Table/string of CSS styles, in the format parseStyles and
+                printStyles produce respectively. Optional, defaults to {}.
+    --]]
     type = function(type, pdfs, classes, styles)
         type = string.fu(string.trim(type or 'Sconosciuto'))
         return type, type, type, pdfs, classes, styles, 'FFF'
     end,
 
+    --[[
+        Box displaying anegg group. Arguments:
+            - egg: The egg group, without ' (gruppo uova)' suffix. Defaults to
+                'Sconosciuto (gruppo uova)'
+            - pdfs: Table or space-spearated string of predefined
+                configurations names. Optional, defaults to {}.
+            - classes: Table/string of CSS classes, in the format parseClasses
+                and printClasses produce respectively. Optional, defaults to {}.
+            - styles: Table/string of CSS styles, in the format parseStyles and
+                printStyles produce respectively. Optional, defaults to {}.
+    --]]
     egg = function(egg, pdfs, classes, styles)
         egg = string.fu(string.trim(egg or 'Sconosciuto (gruppo uova)'))
         return egg, egg .. ' (gruppo uova)', egg .. '_uova', pdfs,
@@ -135,6 +221,7 @@ b.shortHands = {
 --[[
 
 Main function creating a box. Lua interface. Arguments:
+
 - text: displayed text.
 - link: link target, Defaults to text.
 - color: gradient color, from dark to normal, right-to-left.
@@ -163,35 +250,38 @@ b.box_lua = b.boxLua
 
 --[[
 
-Wikicode interface to boxLua. Arguments:
-
-- 1: displayed text.
-- 2: link target, Defaults to text.
-- 3: gradient color, from dark to normal, right-to-left.
-- 4: Space-spearated string of predefined configurations names. Optional,
-    defaults to no predefined configurations.
-- 5: Space-separated list of CSS classes. Optional, defaults to no CSS classes.
-- 6: CSS styles, in the format of an HTML style attribute values. Optional,
-    defaults to no CSS styles.
-- 7: Text color, defaults to #FFFFFF
-
-Example:
-
-{{#invoke | Box | box | erba | erba (gruppo uova) | erba_uova | thin |
-    inline-block width-sm-60 | padding: 2ex; margin-top: 0.5ex; | FFFFFF }}
+Wikicode interface to boxLua. More info (but you won't likely need it if you
+read till here ;) ), in the top comment.
 
 --]]
 b.box = makeWikicodeIntreface(b.boxLua)
 b.Box = b.box
 
+--[[
+    Generating exports for the shorthands: both lua and wikicode interfaces,
+    both for list and single boxes.
+--]]
 for name, makeBoxArgs in pairs(b.shortHands) do
+
+    --[[
+        Lua interface: just calls b.boxLua with wathever the shorthand returns,
+        given arbitrary arguments.
+    --]]
     local luaFunction = function(...)
         return b.boxLua(makeBoxArgs(...))
     end
     local wikicodeFunction = makeWikicodeIntreface(luaFunction)
 
+    -- Exporting single box interfaces
     export(name, 'box', luaFunction, wikicodeFunction)
 
+    --[[
+        List-of-boxes lua interface: the first argument is a sequence: from
+        each item a box is generted, passing along the resto fo the arguments,
+        that are therefore the same for every item in the sequence.
+
+        The sequence can be a table of strings or a comma-separated string.
+    --]]
     local luaList = function(items, ...)
         local args = {...}
         if type(items) == 'string' then
@@ -203,138 +293,8 @@ for name, makeBoxArgs in pairs(b.shortHands) do
     end
     local wikicodeList = makeWikicodeIntreface(luaList)
 
+    -- Exporting list interfaces
     export(name, 'list', luaList, wikicodeList)
 end
 
---[======[
---[[
-
-Shortcut for creating a type box. Lua interface. Arguments:
-- type: The type, defaults to 'Sconosciuto'
-- pdfs: Table or space-spearated string of predefined configurations names.
-    Optional, defaults to {}.
-- classes: Table/string of CSS classes, in the format parseClasses and
-    printClasses produce respectively. Optional, defaults to {}.
-- styles: Table/string of CSS styles, in the format parseStyles and
-    printStyles produce respectively. Optional, defaults to {}.
-
---]]
-b.typeBoxLua = function(type, pdfs, classes, styles)
-    type = string.fu(string.trim(type or 'Sconosciuto'))
-    return b.boxLua(type, type, type, pdfs, classes, styles, 'FFF')
-end
-b.type_box_lua = b.typeBoxLua
-b.boxTipoLua, b.box_tipo_lua = b.typeBoxLua, b.typeBoxLua
-
---[[
-
-Wikicode interface for typeBoxLua. Arguments:
-
-- 1: The type. Defaults to 'Sconosciuto'.
-- 2: Space-spearated string of predefined configurations names. Optional,
-    defaults to no predefined configurations.
-- 3: Space-separated list of CSS classes. Optional, defaults to no CSS classes.
-- 4: CSS styles, in the format of an HTML style attribute values. Optional,
-    defaults to no CSS styles.
-
-Example:
-
-{{#invoke | Box | typeBox | Elettro | thick | inline-block | margin: 3em; }}
-
---]]
-b.typeBox = function(frame)
-    local p = w.trimAll(mw.clone(frame.args), false)
-    return b.typeBoxLua(unpack(p))
-end
-b.TypeBox, b.boxTipo, b.BoxTipo = b.typeBox, b.typeBox, b.typeBox
-
---[[
-
-Shortcut for creating a list of type boxes. Arguments:
-- types: The list of types, as a table of strings.
-- pdfs: Table or space-spearated string of predefined configurations names.
-    Used for every box in the list. Optional, defaults to {}.
-- classes: Table/string of CSS classes, in the format parseClasses and
-    printClasses produce respectively. Used for every box in the list.
-    Optional, defaults to {}.
-
-- styles: Table/string of CSS styles, in the format parseStyles and
-    printStyles produce respectively. Optional, defaults to {}.
-
---]]
-b.typeListLua = function(types, pdfs, class, style)
-    return table.concat(table.map(types, function(type)
-        return b.typeBoxLua(type, pdfs, class, style)
-    end))
-end
-b.type_list_lua = b.typeListLua
-b.listTipoLua, b.list_tipo_lua = b.typeListLua, b.typeListLua
-
---[[
-
-Wikicode interface for typeBoxLua. Arguments:
-
-- 1: The list of types, as a comma-separated list. Spaces are ignored.
-- 2: The predefined configuration names, as a space-separated list of string.
-    Used for every box. Defaults to no predefined configuration.
-- 3: CSS classes, as space-separated list. Used for every box.
-    Defaults to no CSS class.
-- 4: CSS styles, as an HTML style attribute value. Used for every box.
-    Defaults to no CSS styles.
-
-Example:
-
-{{#invoke: Box | typeList | Normale, Acciaio,Terra | tiny | inline-block
-        | padding: 2px; }}
-
---]]
-b.typeList = function(frame)
-    local p = w.trimAll(mw.clone(frame.args), false)
-    local types = mw.text.split(table.remove(p, 1), ',%s*')
-    return b.typeListLua(types, unpack(p))
-end
-b.TypeList, b.listTipo, b.ListTipo = b.typeList, b.typeList, b.typeList
-
---[[
-
-Shortcut for creating an egg group box. Lua interface. Arguments:
-- egg: The egg group, defaults to 'Sconosciuto (gruppo uova)'
-- pdfs: Table or space-spearated string of predefined configurations names.
-    Optional, defaults to {}.
-- classes: Table/string of CSS classes, in the format parseClasses and
-    printClasses produce respectively. Optional, defaults to {}.
-- styles: Table/string of CSS styles, in the format parseStyles and
-    printStyles produce respectively. Optional, defaults to {}.
-
---]]
-b.eggBoxLua = function(egg, pdfs, classes, styles)
-    egg = string.fu(string.trim(egg or 'Sconosciuto (gruppo uova)'))
-    return b.boxLua(egg, egg .. ' (gruppo uova)', egg .. '_uova', pdfs,
-        classes, styles, 'FFF')
-end
-b.egg_box_lua = b.eggBoxLua
-
---[[
-
-Wikicode interface for eggBoxLua. Arguments:
-
-- 1: The egg group. Defaults to 'Sconosciuto (gruppo uova)'.
-- 2: Space-spearated string of predefined configurations names. Optional,
-    defaults to no predefined configurations.
-- 3: Space-separated list of CSS classes. Optional, defaults to no CSS classes.
-- 4: CSS styles, in the format of an HTML style attribute values. Optional,
-    defaults to no CSS styles.
-
-Example:
-
-{{#invoke | Box | eggBox | Erba | thick | inline-block | margin: 3em; }}
-
---]]
-b.eggBox = function(frame)
-    local p = w.trimAll(mw.clone(frame.args), false)
-    return b.eggBoxLua(unpack(p))
-end
-b.EggBox = b.eggBox
-
---]======]
 return b
