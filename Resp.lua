cef5fc253dc5477eec67eb6ba43ebebc017b230c
Davide Laezza <truzzialrogo@gmx.com>
2018-07-25 11:01:32 +0200

    Reverting Resp module to before refactoring

--- Resp.lua
+++ Resp.lua
@@ -129,25 +129,6 @@ end
 
 --[[
 
-This function returns the wikicode interface for a given lua one.
-
-Unlike Wikilib.stdWikicodeInterface, it does not map the empty string to nil:
-in fact, the predefinite styles list of the Box module can be empty, and it is
-in such cases that classes and styles are more important. However, with empty
-string mapped to nil, classes and styles wouldn't be unpacked and passed to
-the lua function, meaning that the box would basically be unstyled.
-
---]]
-local makeWikicodeIntreface = function(luaFunction)
-    return function(frame)
-        local p = w.trimAll(table.copy(frame.args), false)
-        return luaFunction(unpack(p))
-    end
-end
-
-
---[[
-
 This function returns the HTML code for two responsive boxe, given their
 respective arguments passed as tables. For more information about responsive
 boxes, read the comment to responsive.twoBoxes above.
@@ -212,20 +193,6 @@ r.twoCellsLua = function(cell1, cell2, pdfs, bp, classes, styles)
 end
 r.two_cells_lua = r.twoCellsLua
 
-for name, makeBoxArgs in pairs(b.shortHands) do
-    local twoBoxesLua = function(box1, box2, bp, concat, ...)
-        box1 = {makeBoxArgs(box1, ...)}
-        box2 = {makeBoxArgs(box2, ...)}
-        return r.twoBoxesLua(box1, box2, bp, concat)
-    end
-    local twoBoxes = makeWikicodeIntreface(twoBoxesLua)
-
-    local twoCellsLua = function(box1, box2, bp, concat, boxes, cells)
-    end
-
-
-end
-
 --[[
 
 Shortcut to return two responsive type boxes. For more information about
@@ -253,10 +220,12 @@ Arguments:
 --]]
 r.twoTypeBoxesLua = function(type1, type2, pdfs, bp, classes, styles, concat)
     local hasTwoTypes = type2 and type1 ~= type2
+    type1 = string.fu(string.trim(type1 or 'Sconosciuto'))
+    type2 = hasTwoTypes and string.fu(string.trim(type2))
 
-    local box1 = {box.shortHand.type(type1, pdfs, classes, styles)}
-    local box2 = hasTwoTypes and
-            {box.shortHand.type(type2, pdfs, classes, styles)}
+    local box1 = {type1, type1, type1, pdfs, classes, styles, 'FFF'}
+    local box2 = hasTwoTypes and {type2, type2, type2, pdfs, classes, styles,
+        'FFF'}
 	return r.twoBoxesLua(box1, box2, bp, concat)
 end
 r.two_type_boxes_lua = r.twoTypeBoxesLua
