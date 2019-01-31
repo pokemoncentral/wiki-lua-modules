8d19f7cf2f6f044e104a9cfad54ce4b9b9c1910c
Davide Laezza <truzzialrogo@gmx.com>
2019-01-31 20:01:54 +0100

    Fixing '_abbr' function in abbreviation modules breaking their respective data modules

--- Sup-data.lua
+++ Sup-data.lua
@@ -1,8 +1,19 @@
 -- Una tabella con tutti i valori del modulo Sup, per invocazioni interne
 
-local tab = require('Wikilib-tables')
+local h = {}
 local sup = require('Sup')
 
-return table.map(sup, function(value)
-		return value{args={}}
-	end)
+-- Not using map + filter to avoid creating a new table, as b is huge.
+for a, s in pairs(sup) do
+
+    --[[
+        By our internal conventions, functions beginning with an _ are not
+        meant to be called with no arguments, neither to be included in this
+        data module
+    --]]
+    if not a:find('^_+') then
+        h[a] = s{args={}}
+    end
+end
+
+return h
