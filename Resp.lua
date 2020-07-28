650f3dbd937cd332f8e8aced3e43e2a188435496
Davide Laezza <truzzialrogo@gmx.com>
2020-07-28 19:54:06 +0200

    Updating comments in Resp.lua

--- Resp.lua
+++ Resp.lua
@@ -83,23 +83,30 @@ one cell is passed, some styles will be different. In particular, the maximum
 width is set ot 70 on mobile, while when both boxes are present their maximum
 mobile width is set to 35.
 
+TODO: document that if cell1 and cell2 are equal then it's the same as not
+passing cell2
+
 "Responsive cells" means that they stack on desktops but they are side-by-side
 on mobiles. As counter-intuitive as it might sound, it is assumed that the two
 cells should be on a line on their own on mobiles.
+
+Arguments are passed as a single table, both positionally, by using numeric
+keys, and named, by using string keys. Arguments names for named calls are the
+ones listed below, while the order for positional calls is the one they are
+listed in.
 
 Arguments:
     - cell1: The content of the first cell, as a string.
     - cell2: The content of the second cell, as a string. Any value evaluating
         to false will trigger the single-cell styles for cell1.
-    - pdfs: Table or space-spearated string of predefined configurations names,
-        to be used for both cells. Optional, defaults to {}.
-    - bp: The breakpont the responsive design is triggered at. Defaults to xs.
-    - classes: Table/string of CSS classes, in the format parseClasses and
-        printClasses produce respectively. Used for both cells. Optional,
-        defaults to {}.
-    - styles: Table/string of CSS styles, in the format parseStyles and
-        printStyles produce respectively. Used for both cells. Optional,
-        defaults to {}.
+    - confs, pdfs: Table or space-spearated string of predefined configurations
+    names, to be used for both cells. Optional, defaults to {}.
+    - classes: A list of CSS classes to be used for both cells. Any format
+        parseClasses takes in is accepted. Optional, defaults to {}.
+    - styles: Some CSS styles to be used for both cells. Any format
+        parseStyles takes in is accepted. Optional, defaults to {}.
+    - bp: The breakpont the responsive design is triggered at. Defaults to
+        'xs'.
 
 Return:
     - 1: Complete wikicode table-cell containing cell1 adjusted to be
@@ -166,18 +173,17 @@ end
 
 --[[
 
-This function returns the HTML code for two responsive boxe, given their
-respective arguments passed as tables. For more information about responsive
-boxes, read the comment to responsive.twoBoxes above.
+This function returns the HTML code for two responsive boxes, given two
+BoxClass instances. For more information about responsive boxes, read the
+comment to responsive.twoBoxes above.
 
 The two boxes can be returned as two separate stirngs or be concatenated
 together. If the second box is not passed, only the first one is returned.
 
 Arguments:
-    - box1: A table containing the arguments to boxLua in Box modules.
-    - box2: A table containing the arguments to boxLua in Box modules.
-        Any value evaluating to false will trigger the single-box styles
-        for box1.
+    - box1: A BoxClass instance, as defined in the Box module.
+    - box2: A BoxClass instance, as defined in the Box module. Any value
+        evaluating to false will trigger the single-box styles for box1.
     - bp: The breakpont the responsive design is triggered at. Defaults to xs.
     - concat: whether the two boxes should be concatenated or they should be
         returned as two seaprate strings. Defaults to true.
@@ -199,8 +205,14 @@ r.twoBoxesLua = function(box1, box2, bp, concat)
 end
 r.two_boxes_lua = r.twoBoxesLua
 
--- NOTE: no intentional wikicode interface. Really too complex to be worth it,
--- there's no way to group parameters for a single box.
+--[[
+
+NOTE: don't worry, the wikicode version is planned. It will just use the same
+classes, styles, confs... for both boxes for the sake of simplicity. I mean,
+the fact that in lua you can have them different for the two boxes is more like
+a nice-to-have that is also convenient to implement than an actual necessity.
+
+--]]
 
 --[[
 
@@ -209,19 +221,25 @@ content. For more information about responsive cells, read the comment to
 responsive.twoCells above. The two cells are concatenated together in one
 Wikicode string.
 
+TODO: document that if cell1 and cell2 are equal then it's the same as not
+passing cell2
+
+Arguments are passed as a single table, both positionally, by using numeric
+keys, and named, by using string keys. Arguments names for named calls are the
+ones listed below, while the order for positional calls is the one they are
+listed in.
+
 Arguments:
     - cell1: The content of the first cell, as a string.
     - cell2: The content of the second cell, as a string. Any value evaluating
         to false will trigger the single-cell styles for cell1.
-    - pdfs: Table or space-spearated string of predefined configurations names,
-        to be used for both cells. Optional, defaults to {}.
-    - bp: The breakpont the responsive design is triggered at. Defaults to xs.
-    - classes: Table/string of CSS classes, in the format parseClasses and
-        printClasses produce respectively. Used for both cells. Optional,
-        defaults to {}.
-    - styles: Table/string of CSS styles, in the format parseStyles and
-        printStyles produce respectively. Used for both cells. Optional,
-        defaults to {}.
+    - confs, pdfs: Table or space-spearated string of predefined configurations
+    names, to be used for both cells. Optional, defaults to {}.
+    - classes: A list of CSS classes to be used for both cells. Any format
+        parseClasses takes in is accepted. Optional, defaults to {}.
+    - styles: Some CSS styles to be used for both cells. Any format
+        parseStyles takes in is accepted. Optional, defaults to {}.
+    - bp: The breakpont the responsive design is triggered at. Defaults to 'xs'.
 
 --]]
 r.twoCellsLua = function(args)
@@ -231,30 +249,44 @@ r.two_cells_lua = r.twoCellsLua
 
 --[[
 
+NOTE: Don't worry, the wikicode version is coming. I just forgot about it -_-"
+
+--]]
+
+--[[
+
 Shortcut to return two responsive type boxes. For more information about
 responsive boxes, read the comment to responsive.twoBoxes above. The first
 type will have single-box styles if the second one is not passed, or it is
 equal to the first type. The two boxes can be concatenated or returned as two
 strings.
 
+Arguments are passed as a single table, both positionally, by using numeric
+keys, and named, by using string keys. Arguments names for named calls are the
+ones listed below, while the order for positional calls is the one they are
+listed in.
+
 Arguments:
     - type1: First type.
     - type2: Second type. Any value evaluating to false or equal to type1
         will trigger the single-box styles for type1.
-    - pdfs: Table or space-spearated string of predefined configurations names,
-        to be used for both cells. Optional, defaults to {}.
+    - confs, pdfs: Table or space-spearated string of predefined configurations
+        names, to be used for both boxes. Optional, defaults to {}.
+    - classes: A list of CSS classes to be used for both boxes. Any format
+        parseClasses takes in is accepted. Optional, defaults to {}.
+    - styles: Some CSS styles to be used for both boxes. Any format
+        parseStyles takes in is accepted. Optional, defaults to {}.
     - bp: The breakpont the responsive design is triggered at. Defaults to xs.
-    - classes: Table/string of CSS classes, in the format parseClasses and
-        printClasses produce respectively. Used for both cells. Optional,
-        defaults to {}.
-    - styles: Table/string of CSS styles, in the format parseStyles and
-        printStyles produce respectively. Used for both cells. Optional,
-        defaults to {}.
     - concat: whether the two boxes should be concatenated or they should be
         returned as two seaprate strings. Defaults to true.
 
 --]]
 r.twoTypeBoxesLua = function(args)
+    --[[
+        Table.remove is called twice with index 1 because it is now the
+        original second argument. This is due to the first original argument
+        being removed by the first call to table.remove
+    --]]
     local type1 = args.type1 or table.remove(args, 1)
     local type2 = args.type2 or table.remove(args, 1)
     local bp = args.bp or args[6]
@@ -262,6 +294,15 @@ r.twoTypeBoxesLua = function(args)
 
     local hasTwoTypes = type2 and type1 ~= type2
 
+    --[[
+        We need to pass in the type as the first argument, rather than as a
+        named parameter, because it was removed from `args` a few lines above.
+        This shifted all the other possible positional parameters in `args`
+        by one position up, which creates an off-by-one offset in the
+        positional arguments list. Therefore if we don't add the first
+        positional parameter back, we break the positional argument passing
+        altogether.
+    --]]
     local box1 = box.shortHand.type(table.merge({type1}, args))
     local box2 = type2
     if hasTwoTypes then
@@ -272,13 +313,64 @@ r.twoTypeBoxesLua = function(args)
 end
 r.two_type_boxes_lua = r.twoTypeBoxesLua
 
+--[[
 
+Shortcut to return two responsive type boxes. For more information about
+responsive boxes, read the comment to responsive.twoBoxes above. The first
+type will have single-box styles if the second one is not passed, or it is
+equal to the first type.
+
+Arguments can be both named or positional (unnamed) Arguments names for named
+calls are the ones listed below, while the order for positional calls is the
+one they are listed in.
+
+Arguments:
+    - type1: First type.
+    - type2: Second type. Any value evaluating to false or equal to type1
+        will trigger the single-box styles for type1.
+    - confs, pdfs: Space-spearated list of predefined configurations names,
+        to be used for both boxes. Optional, defaults to an empty list.
+    - classes: Space-spearated list of CSS classes to be used for both boxes.
+        Optional, defaults to an empty list.
+    - styles: Some CSS styles to be used for both boxes. Optional, defaults to
+        no styles.
+    - bp: The breakpont the responsive design is triggered at. Defaults to xs.
+
+Example invocations:
+    - {{#invoke: Resp | twoTypeBoxes | type1 = Elettro | type2 = Buio |
+        confs = thick }}
+    - {{#invoke: Resp | twoTypeBoxes | Erba | Lotta | classes = roundy-20 |
+        styles = padding: 0.3em 0.5ex }}
+
+--]]
 r.twoTypeBoxes = function(frame)
     local p = w.trimAll(frame.args, false)
     p.concat = true
     return r.twoTypeBoxes(p)
 end
 
+--[[
+
+Shortcut method returning two responsive cells containing two type boxes. For
+more information about responsive cells, read the comment to
+responsive.twoCells above. The first cell will have single-cell styles if the
+second type is not passed, or it is equal to the first one.
+
+Arguments are passed as a single table, both positionally, by using numeric
+keys, and named, by using string keys. Arguments names for named calls are the
+ones listed below, while the order for positional calls is the one they are
+listed in.
+
+Arguments:
+    - type1: First type.
+    - type2: Second type. Any value evaluating to false or equal to type1
+        will trigger the single-cell styles for the first cell.
+    - boxArgs: Table containing the arguments to configure the boxes for both
+        type1 and type2. Can be anything the BoxClass constructor takes in.
+    - cellArgs: Table containing the arguments to configure both the cells. Can
+        be any argument `twoCellsLua` takes in, except for the cells content.
+
+--]]
 r.twoTypeCellsLua = function(args)
     local type1 = args.type1 or args[1]
     local type2 = args.type2 or args[2]
@@ -288,6 +380,15 @@ r.twoTypeCellsLua = function(args)
 
     local hasTwoTypes = type2 and type1 ~= type2
 
+    --[[
+        We need to pass in the type as the first argument, rather than as a
+        named parameter, because it was removed from `args` a few lines above.
+        This shifted all the other possible positional parameters in `args`
+        by one position up, which creates an off-by-one offset in the
+        positional arguments list. Therefore if we don't add the first
+        positional parameter back, we break the positional argument passing
+        altogether.
+    --]]
     local box1 = box.shortHand.type(table.merge({type1}, boxArgs))
     local box2 = type2
     if hasTwoTypes then
@@ -302,6 +403,14 @@ r.twoTypeCellsLua = function(args)
 	return responsive.twoBoxesLua(twoCellArgs)
 end
 
+--[[
+
+Returns the subtable whose keys have a given prefix. Such keys will have the
+prefix stripped (e.g. {hodAA = 4, hodBB = 0, rikf = 3} --> {aA = 4, bB = 0})
+
+TODO: Proper comments here
+
+--]]
 local pairsWithPrefixStripped = function(items, prefix)
     local filtered = table.filter(items, function(_, key)
         return key:find('^' .. prefix) end)
@@ -317,6 +426,34 @@ local pairsWithPrefixStripped = function(items, prefix)
     return res
 end
 
+--[[
+
+Shortcut method returning two responsive cells containing two type boxes. For
+more information about responsive cells, read the comment to
+responsive.twoCells above. The first cell will have single-cell styles if the
+second type is not passed, or it is equal to the first one.
+
+Arguments are only named. Some of the argument names follow a specific scheme:
+- Argument names starting with 'box' are used to configure the two boxes. They
+    can end with any of the named arguments accepted by the box function in the
+    Box module. For example, 'boxClasses' or 'boxConfs'.
+- Argument names starting with 'cell' are used to configure the two cells. They
+    can end with any of the named arguments accepted by the twoCells function
+    in this module (Resp). For example, 'cellClasses' or 'cellStyles'.
+
+Arguments:
+    - type1: First type.
+    - type2: Second type. Any value evaluating to false or equal to type1
+        will trigger the single-cell styles for the first cell.
+    - box*: Arguments for configuring both the boxes. Read above for more info.
+    - cell*: Arguments for configuring both the cells. Read above for more
+        information.
+
+Examples:
+    {{#invoke: Resp | twoTypeCells | type1 = Elettro | type2 = Lotta |
+        boxConfs = thick | cellStyles = padding: 0.3ex }}
+
+--]]
 r.twoTypeCells = function(frame)
     local type1 = frame.args.type1
     local type2 = frame.args.type2
@@ -373,31 +510,6 @@ r.twoEggBoxesLua = function(egg1, egg2, pdfs, bp, classes, styles, concat)
 	return r.twoBoxesLua(box1, box2, bp, concat)
 end
 r.two_egg_boxes_lua = r.twoEggBoxesLua
-
---[[
-
-Shortcut method returning two responsive cells containing two type boxes. For
-more information about responsive cells, read the comment to
-responsive.twoCells above. The first cell will have single-cell styles if the
-second type is not passed, or it is equal to the first one.
-
-Arguments:
-    - type1: First type.
-    - type2: Second type. Any value evaluating to false or equal to type1
-        will trigger the single-cell styles for the first cell.
-    - types: Table containing the arguments for typeBoxLua in the Box module,
-        other than the type. Used for both type1 and type2.
-    - cells: Table containing the arguments for twoCellsLua.
-
---]]
-r.twoTypeCellsLua = function(type1, type2, types, cells)
-    local box1 = box.typeBoxLua(type1, table.unpack(types))
-    local box2 = type2
-                 and type1 ~= type2
-                 and box.typeBoxLua(type2, table.unpack(types))
-    return r.twoCellsLua(box1, box2, table.unpack(cells))
-end
-r.two_type_cells_lua = r.twoTypeCellsLua
 
 --[[
 
